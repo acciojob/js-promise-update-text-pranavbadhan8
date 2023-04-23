e6b02519a1baaa96cb72b3ba7d55297bd9aeba51
pranavbadhan8 <120732731+pranavbadhan8@users.noreply.github.com>
2023-04-23 20:16:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 //your JS code here. If rlet output = document.getElementById("output");
 let p = new Promise((resolve,reject) =>{
-	setInterval(resolve("'Hello, world!'"),1000);
+	setTimeout=>(resolve("'Hello, world!'"),1000);
 })
 p.then((value)=>{
 	output.innerHTML = value;
