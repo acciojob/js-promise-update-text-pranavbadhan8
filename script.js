94b69a13fde139b22709f80866cc01bcf5f390ce
pranavbadhan8 <120732731+pranavbadhan8@users.noreply.github.com>
2023-04-23 20:15:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-//your JS code here. If required.
+//your JS code here. If rlet output = document.getElementById("output");
+let p = new Promise((resolve,reject) =>{
+	setInterval(resolve("'Hello, world!'"),1000);
+})
+p.then((value)=>{
+	output.innerHTML = value;
+})
